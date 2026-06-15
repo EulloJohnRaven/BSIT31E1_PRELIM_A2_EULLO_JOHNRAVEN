4802ca57e543ad2bf6e9ea1cf13fa66bb9421a4e
EulloJohnRaven <cancergaming123456@gmail.com>
2026-06-15 13:39:49 +0800

    Template

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.WriteLine("Enter first number (or 'exit' to quit)");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "exit")
+    
